a047c2744b7110a59145436f464a585a8e34aed7
yehli <yehli@RICO-0007>
2023-12-05 03:29:09 +0800

    5th december 330am version

---
+++ iHateJava001/src/Assignment/AdminAll/Admin.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment.AdminAll;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author yehli
+ */
+public class Admin extends JFrame{
+    
+    
+    
+}
+        
